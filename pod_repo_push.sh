b8d666a84ed6d4546a65148ab6fb11b43b041637
Liang's Mac <757437150@qq.com>
2018-01-10 14:58:33 +0800

    pod_push sh update

--- pod_repo_push.sh
+++ pod_repo_push.sh
@@ -1,1 +1,2 @@
-pod repo push HLPodRepo HLExtensions.podspec --verbose --allow-warnings --sources=https://github.com/HuuLiang/HLExtensions.git,https://github.com/CocoaPods/Specs.git
+#pod repo push HLPodRepo HLExtensions.podspec --verbose --allow-warnings --sources=https://github.com/HuuLiang/HLExtensions.git,https://github.com/CocoaPods/Specs.git
+pod repo push HLPodRepo HLExtensions.podspec --verbose --allow-warnings
